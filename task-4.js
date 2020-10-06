9eac6dd9c6eeacdb765066e7ae2c74a1256ce0b0
Oleksandr Slipchenko <o.p.slipchenko@gmail.com>
2020-10-06 19:39:55 +0300

    Добавляет 4ю задачу

--- task-4.js
+++ task-4.js
@@ -2,23 +2,26 @@
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
-let counterValueRef = document.getElementById('value').textContent;
-
-// console.log(counterValueRef);
+const counterValueRef = document.querySelector('#value').textContent;
 
 const btnIncrRef = document.querySelector('[data-action="increment"]');
-// console.log(btnIncrRef);
 
 const btnDecrRef = document.querySelector('[data-action="decrement"]');
 
-// console.log(btnDecrRef);
+let counterValue = Number(counterValueRef);
 
-let counterValue = 0;
+btnIncrRef.addEventListener('click', onIncrement);
 
-btnIncrRef.addEventListener('click', value => {
-  if (value.target) {
-    counterValue += 1;
-    counterValueRef = String(counterValue);
-  }
-  return counterValueRef;
-});
+btnDecrRef.addEventListener('click', decrement);
+
+function onIncrement(event) {
+  counterValue += 1;
+  const result = String(counterValue);
+  document.querySelector('#value').textContent = result;
+}
+
+function decrement(event) {
+  counterValue -= 1;
+  const result = String(counterValue);
+  document.querySelector('#value').textContent = result;
+}
